a6a9006c7b69c602ca6b0d4b75c182124b0ad9df
James Lawrence <jamesblawrence2007@gmail.com>
2025-04-09 09:52:47 -0600

    populated analytics table with data

--- angular-timesheet/src/app/components/analytics-table/analytics-table.component.ts
+++ angular-timesheet/src/app/components/analytics-table/analytics-table.component.ts
@@ -6,7 +6,9 @@ import { Employee } from '../../interfaces/employee'
   templateUrl: './analytics-table.component.html',
   styleUrl: './analytics-table.component.scss'
 })
-export class AnalyticsTableComponent {
+export class AnalyticsTableComponent{
+  @Input() 
+  departmentId: string | undefined;
   weekdays: string[] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
   employees: Employee[] = [];
   employeeData: Employee[] = [
@@ -63,4 +65,12 @@ export class AnalyticsTableComponent {
           wednesday: 5
       }
   ];
+  constructor() {}
+  ngOnInit(): void {
+    this.employees = this.employeeData.filter(employee => employee.departmentId === this.departmentId);
+  }
+  getTotalHours(employee: Employee): number {
+    return employee.monday + employee.tuesday + employee.wednesday
+        + employee.thursday + employee.friday + employee.saturday + employee.sunday;
+  }
 }
